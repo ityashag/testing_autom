bda06fe9cb8458084ec44f196a5f47efebd4f87e
ityashag <97851640+ityashag@users.noreply.github.com>
2024-11-03 19:44:31 +0530

    1

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'Healthy' });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
